e732e241f4522b9bee4a4340021b7ea1b1dfda29
JASIR MOHAMMED <jasirmj@gmail.com>
2022-08-28 14:32:14 +0530

    react router connected

--- src/App.js
+++ src/App.js
@@ -1,29 +1,96 @@
 import logo from './logo.svg';
 import './App.css';
-import Dictaphone from './SpeechToText';
-import SpeechToText2 from './SpeechToText2';
+// import Dictaphone from './SpeechToText';
+// import SpeechToText2 from './SpeechToText2';
+// import {
+//   BrowserRouter as Router,
+//   Routes ,
+//   Route,
+//   Link
+// } from "react-router-dom";
 
-function App() {
+import { BrowserRouter as Router, Route, Routes,Link } from 'react-router-dom'
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p> */}
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Dictaphone />
-        {/* <SpeechToText2/> */}
-      </header>
-    </div>
+    <Router>
+        <Routes>
+          <Route exact path="/medicine/:id" element={<MedicinePage/>}/>
+          <Route exact path="/home" element={<HomePage/>}/>
+          <Route exact path="/" element={<HomePage/>}/>
+
+          <Route path="*" element={<NotFound/>}/>
+        </Routes>
+    </Router>
   );
 }
-
 export default App;
+
+function HomePage(){
+  return (
+    <div>
+    <div className="header">
+      <h1>Medical</h1>
+    </div>
+    <div className="container">
+      <div className="content">
+        <div className="text1">
+          Paracetamol
+        </div>
+        <Link  className="text2" to="/medicine/1">
+          view
+        </Link>
+      </div>
+     
+    </div>
+  </div>
+  )
+}
+
+
+
+function MedicinePage() {
+  return (
+    <>
+      <div>
+        <div className="header1">
+          <Link to="/home">Back</Link>
+          </div>
+        <div className="data">
+          <form>
+            <label htmlFor="name"> Name:</label><br />
+            <input type="text" id="question" style={{ fontSize: 30 }} /><br />
+            <label htmlFor="question">Question:</label>
+            <div>
+              <input type="button" defaultValue="Yes" className="btn" />
+              <input type="button" defaultValue="No" className="btn" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+
+
+}
+
+function NotFound() {
+  return (
+    <>
+      <div>
+        <div className="header1">
+        <Link to="/home">Back</Link>
+
+          </div>
+        <div className="data">
+          <form>
+           <div>Not Found</div>
+          </form>
+        </div>
+      </div>
+    </>
+  )
+
+
+}
+
